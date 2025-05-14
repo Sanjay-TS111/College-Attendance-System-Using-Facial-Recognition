aeeec3d176b1c351bb50eba0c78533d9b5a26cab
Sanjay-TS111 <tssanjay078@gmail.com>
2025-05-14 10:25:03 +0530

    Fix CSRF issue by adding CSRF_TRUSTED_ORIGINS and secure cookie settings

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['attendance-system.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -137,5 +137,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+CSRF_TRUSTED_ORIGINS = ['https://attendance-system.up.railway.app']
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
 
